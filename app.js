0b3bf59f21272b4d0d8491ca2da2c919a84ee082
erik-r-u-kodehode <125983659+erik-r-u-kodehode@users.noreply.github.com>
2023-04-11 10:08:23 +0200

    Massive updates to code

--- app.js
+++
@@ -1,6 +0,0 @@
-function myFunction(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    document.getElementById("tooltip").style.left = x + "px";
- document.getElementById("tooltip").style.top = y + "px";
-}
---
+++ script.js
@@ -0,0 +1,24 @@
+window.location.hash = '';//remove hash text
+window.location.href.replace('#', '');//remove hash
+history.replaceState(null, null, window.location.href);//replace state
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 620 || document.documentElement.scrollTop > 620) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
